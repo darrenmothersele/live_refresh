5d9589483167bc9a08b4493539124a41be66b4d9
Aegir user <aegir@zoostorm.(none)>
2012-07-31 23:55:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Live blogging for Drupal. An example module. Not fully featured.
+
+For more information see here:
+http://www.darrenmothersele.com/node/145
---
+++ js/live-refresh.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+  Drupal.behaviors.liveRefresh = {
+    attach: function (context, settings) {	
+			var refreshId = setInterval(function() {
+				$.ajax({
+					url: settings.basePath + 'live-refresh-ajax/' + Drupal.settings.liveRefresh.lastCreated,
+					dataType: 'json',
+					success: function(data) {
+						if (data.lastCreated) {
+  						Drupal.settings.liveRefresh.lastCreated = data.lastCreated;
+	  					$('#live-refresh-wrapper', context).prepend(data.build);
+	          }
+					},
+				});
+			}, settings.liveRefresh.refreshRate);
+    }
+  };
+
+})(jQuery);
